b1596d58ffe76072efabd85e71b7323088fab023
ZJendex <zhubean.s@gmail.com>
2020-08-21 17:01:19 -0400

    deploying update

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ let express = require("express"),
     expressSanitizer = require("express-sanitizer");
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
+//Check if DATABASEURL has been initialized in config vars in heroku (app deployed or not)
+//set DATABASEURL locally if it didn't
+if(!process.env.DATABASEURL){
+    process.env.DATABASEURL = "mongodb://localhost:27017/restful_blog_app";
+}
+//connect/initialized the database  --adding options to avoid the errors
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -114,7 +120,7 @@ app.delete("/blogs/:id", function (req, res) {
 });
 
 
-app.listen(2000, function(){
+app.listen(process.env.PORT || 3000, function(){
    console.log("The blog server started!!")
 });
 
